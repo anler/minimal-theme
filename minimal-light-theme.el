2cc688c1705eeb77fe1deeea35bfce378081f238
ikame <anler86@gmail.com>
2014-04-10 01:01:32 +0200

    Major change in colors

--- minimal-light-theme.el
+++ minimal-light-theme.el
@@ -1,4 +1,4 @@
-;;; minimal-theme.el --- A dark minimalistic Emacs 24 theme.
+;;; minimal-light-theme.el --- A light/dark minimalistic Emacs 24 theme.
 
 ;; Copyright (C) 2014 Anler Hp
 
@@ -29,97 +29,144 @@
 (deftheme minimal-light "minimal light theme.")
 
 (let* ((class '((class color) (min-colors 89)))
-      (foreground "black")
-      (background "white")
-      (dark "grey80")
-      (dark-2 "grey95")
-      (dark-3 "ghost white")
-      (light "gray20")
-      (notice "light slate gray")
-      (warning "gold")
-      (code-block `(:foreground ,foreground :background ,dark-2)))
+       (foreground "grey20")
+       (background "white")
+       (cursor "black")
+       (border "grey90")
+       (minibuffer cursor)
+       (region "grey90")
+       (comment-delimiter "grey85")
+       (comment "grey50")
+       (constant foreground)
+       (string "grey40")
+       (modeline-foreground foreground)
+       (modeline-background "grey95")
+       (modeline-foreground-inactive comment)
+       (modeline-background-inactive background)
+       (hl-background region)
+       (hl-face-background nil)
+       (failure "red")
+       (org-background "grey98")
+       )
+  (setq fci-rule-color comment)
   (custom-theme-set-faces
    'minimal-light
 
    ;; basic stuff
-   `(default ((t (:background ,background :foreground ,foreground))))
-   `(cursor ((t (:background ,warning :inverse-video t))))
-   `(shadow ((t (:foreground ,light))))
-   `(fixed-pitch ((t (:family "Monospace"))))
-   `(variable-pitch ((t (:family "Sans Serif"))))
-   `(escape-glyph ((t (:foreground ,light))))
+   `(default ((,class (:background ,background :foreground ,foreground))))
+   `(cursor ((,class (:background ,cursor :inverse-video t))))
+   `(vertical-border ((,class (:foreground ,border))))
 
-   `(linum ((t (:background ,background :foreground ,dark))))
-   `(vertical-border ((t (:foreground ,dark))))
+   ;; minibuffer
+   `(minibuffer-prompt ((,class (:foreground ,minibuffer :weight bold))))
 
-   `(minibuffer-prompt ((t (:foreground ,foreground :weight bold))))
+   ;; region
+   `(region ((,class (:background ,region))))
+   `(secondary-selection ((,class (:background ,region))))
 
-   `(highlight ((t (:foreground ,background :background ,light))))
-   `(region ((t (:foreground ,background :background ,light))))
-   `(secondary-selection ((t (:background ,dark-3))))
+   ;; faces
+   `(font-lock-builtin-face ((,class (:foreground ,foreground :weight bold))))
+   `(font-lock-constant-face ((,class (:foreground ,foreground :weight bold))))
+   `(font-lock-keyword-face ((,class (:foreground ,foreground :weight bold))))
+   `(font-lock-type-face ((,class (:foreground ,foreground :slant italic))))
+   `(font-lock-function-name-face ((,class (:foreground ,foreground :weight bold))))
+   `(font-lock-variable-name-face ((,class (:foreground ,foreground))))
 
-   `(fci-rule-color ,dark)
+   `(font-lock-comment-delimiter-face ((,class (:foreground ,comment-delimiter))))
+   `(font-lock-comment-face ((,class (:foreground ,comment))))
+   `(font-lock-doc-face ((,class (:inherit (font-lock-comment-face)))))
+   `(font-lock-string-face ((,class (:foreground ,foreground :foreground ,string))))
 
-   ;; magit
-   `(magit-diff-use-overlays nil)
+   ;; faces used by isearch
+   `(isearch ((,class (:foreground ,foreground :background ,region :weight normal))))
+   `(isearch-fail ((,class (:foreground ,failure :bold t))))
+   `(lazy-highlight
+     ((,class (:foreground ,foreground :background ,region))))
 
-   ;; whitespace
-   `(trailing-whitespace ((t (:background "red"))))
+   ;; ido-mode
+   `(ido-subdir ((,class (:foreground ,foreground :weight bold))))
+   `(ido-only-match ((,class (:foreground ,foreground :weight bold))))
 
-   `(font-lock-builtin-face ((t (:foreground ,foreground :weight bold))))
-   `(font-lock-comment-delimiter-face ((default (:inherit (font-lock-comment-face)))))
-   `(font-lock-comment-face ((t (:foreground ,dark :slant italic))))
-   `(font-lock-constant-face ((t (:foreground ,foreground :weight bold))))
-   `(font-lock-doc-face ((t (:inherit (font-lock-string-face)))))
-   `(font-lock-keyword-face ((t (:weight bold :foreground ,foreground))))
-   `(font-lock-negation-char-face ((t nil)))
-   `(font-lock-preprocessor-face ((t (:inherit (font-lock-builtin-face)))))
-   `(font-lock-regexp-grouping-backslash ((t (:inherit (bold)))))
-   `(font-lock-regexp-grouping-construct ((t (:inherit (bold)))))
-   `(font-lock-string-face ((t (:foreground ,light))))
-   `(font-lock-type-face ((t (:foreground ,foreground :slant italic))))
-   `(font-lock-function-name-face ((t (:foreground ,foreground :slant italic))))
-   `(font-lock-variable-name-face ((t (:foreground ,foreground :weight bold))))
-   `(font-lock-warning-face ((t (:foreground ,warning))))
+   ;; show-paren
+   `(show-paren-match
+     ((,class (:background ,region))))
+   `(show-paren-mismatch
+     ((,class (:foreground ,failure :weight bold))))
 
-   '(button ((t (:inherit link))))
-
-   `(link ((t (:foreground ,light :underline t))))
-   `(link-visited ((t (:foreground ,notice :underline t))))
-
-   `(fringe ((t (:background ,background :foreground ,foreground))))
-
+   ;; modeline
    `(mode-line
-     ((t (:box (:line-width -1 :color ,background :style released-button)
-               :foreground ,foreground :background ,dark))))
-   `(mode-line-buffer-id ((t (:weight bold))))
-   `(mode-line-emphasis ((t (:weight bold))))
-   `(mode-line-highlight
-     ((((class color) (min-colors 88))
-       (:box (:line-width 2 :color ,dark :style released-button)))
-      (t (:inherit (highlight)))))
-   `(mode-line-inactive
-     ((t (:box (:line-width -1 :color ,background :style nil) :foreground ,dark :background nil))))
+       ((,class (:inverse-video unspecified
+                                :overline ,border
+                                :underline nil
+                                :foreground ,modeline-foreground
+                                :background ,modeline-background
+                                :box (:line-width 1 :color ,background :style unspecified)
+                                ))))
+     `(mode-line-buffer-id ((,class (:weight bold))))
+     `(mode-line-inactive
+       ((,class (:inverse-video unspecified
+                                :overline ,border
+                                :underline nil
+                                :foreground ,modeline-foreground-inactive
+                                :background ,modeline-background-inactive
+                                :box (:line-width 1 :color ,border :style unspecified)
+                                ))))
 
-   `(lazy-highlight ((t (:foreground ,background :background ,light))))
-   '(next-error ((t (:inherit (region)))))
-   '(query-replace ((t (:inherit (isearch)))))
+      ;; hl-line-mode
+     `(hl-line ((,class (:background ,hl-background))))
+     `(hl-line-face ((,class (:background ,hl-face-background))))
 
-   `(org-level-1 ((t (:bold t :foreground ,notice :height 1.5))))
-   `(org-level-2 ((t (:bold t :foreground ,notice :height 1.2))))
-   `(org-level-3 ((t (:bold t :foreground ,notice :height 1.0))))
-   `(org-level-4 ((t (:bold t :foreground ,notice :height 1.0))))
-   `(org-link ((t (:underline t))))
-   `(org-todo ((t (:bold t :foreground "red"))))
-   `(org-done ((t (:bold t :foreground "green"))))
-   `(org-block-background ((,class (:background ,dark-2))))
-   `(org-block-begin-line ((,class (:inherit code-block :overline ,dark :slant italic))))
-   `(org-block-end-line ((,class (:inherit code-block :underline ,dark :slant italic))))
+     ;; org-mode
+     `(org-level-1 ((,class (:foreground ,foreground :height 1.6))))
+     `(org-level-2 ((,class (:foreground ,foreground :height 1.5))))
+     `(org-level-3 ((,class (:foreground ,foreground :height 1.4))))
+     `(org-level-4 ((,class (:foreground ,foreground :height 1.3))))
+     `(org-level-5 ((,class (:foreground ,foreground :height 1.2))))
+     `(org-level-6 ((,class (:foreground ,foreground :height 1.1))))
+     `(org-level-7 ((,class (:foreground ,foreground))))
+     `(org-level-8 ((,class (:foreground ,foreground))))
 
-   ;; ido
-   `(ido-first-match ((t (:foreground ,foreground :underline t))))
-   `(ido-only-match ((t (:foreground ,foreground :underline t :weight bold))))
-   `(ido-subdir ((t (:foreground ,foreground :weight bold))))))
+     ;; outline
+     `(outline-1 ((,class (:inherit org-level-1))))
+     `(outline-2 ((,class (:inherit org-level-2))))
+     `(outline-3 ((,class (:inherit org-level-3))))
+     `(outline-4 ((,class (:inherit org-level-4))))
+     `(outline-5 ((,class (:inherit org-level-5))))
+     `(outline-6 ((,class (:inherit org-level-6))))
+     `(outline-7 ((,class (:inherit org-level-7))))
+     `(outline-8 ((,class (:inherit org-level-8))))
+
+     `(org-document-title ((,class (:foreground ,foreground))))
+
+     `(org-link ((,class (:background ,org-background :foreground ,foreground :underline t))))
+     `(org-tag ((,class (:background ,org-background :foreground ,foreground))))
+     `(org-warning ((,class (:background ,region :foreground ,foreground :weight bold))))
+     `(org-todo ((,class (:background ,region :foreground ,foreground :weight bold))))
+     `(org-done ((,class (:background ,region :foreground ,foreground :weight bold))))
+
+     `(org-table ((,class (:background ,org-background))))
+     `(org-code ((,class (:background ,org-background))))
+     `(org-date ((,class (:background ,org-background :underline t))))
+     `(org-block ((,class (:background ,org-background))))
+     `(org-block-background ((,class (:background ,org-background :foreground ,foreground))))
+     `(org-block-begin-line
+       ((,class (:background ,org-background :foreground ,comment-delimiter :weight bold))))
+     `(org-block-end-line
+       ((,class (:background ,org-background :foreground ,comment-delimiter :weight bold))))
+
+     ;; js2-mode
+     `(js2-external-variable ((,class (:inherit base-faces :weight bold))))
+     `(js2-function-param ((,class (:inherit base-faces))))
+     `(js2-instance-member ((,class (:inherit base-faces))))
+     `(js2-jsdoc-html-tag-delimiter ((,class (:inherit base-faces))))
+     `(js2-jsdoc-html-tag-name ((,class (:inherit base-faces))))
+     `(js2-jsdoc-tag ((,class (:inherit base-faces))))
+     `(js2-jsdoc-type ((,class (:inherit base-faces :weight bold))))
+     `(js2-jsdoc-value ((,class (:inherit base-faces))))
+     `(js2-magic-paren ((,class (:underline t))))
+     `(js2-private-function-call ((,class (:inherit base-faces))))
+     `(js2-private-member ((,class (:inherit base-faces))))
+   ))
 
 ;;;###autoload
 (when (and (boundp 'custom-theme-load-path) load-file-name)
